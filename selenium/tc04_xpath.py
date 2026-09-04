64c0863f79dc735763171253b67f19f3689798a0
iamcody-git <iamcody.github@gmail.com>
2026-09-04 10:20:29 +0545

    automation with seleinum

--- selenium/tc04_xpath.py
+++ selenium/tc04_xpath.py
@@ -1,6 +1,3 @@
-
-# about xpath absolue and relative path
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
@@ -11,20 +8,56 @@ driver.maximize_window()
 driver.get("https://www.saucedemo.com/")
 time.sleep(2)
 
-#locating username by relative xpath
-username = driver.find_element(By.XPATH, value="//input[@id='user-name']")
-password = driver.find_element(By.XPATH, value="//input[@name='password']")
+# Username
+username = driver.find_element(
+    By.XPATH,
+    "//input[@id='user-name']"
+)
 
+# Password
+password = driver.find_element(
+    By.XPATH,
+    "//input[@name='password']"
+)
+
+# Enter login details
 username.send_keys("standard_user")
-time.sleep(2)
-#enter password get from website 
 password.send_keys("secret_sauce")
-time.sleep(5)
 
-button = driver.find_element(By.XPATH,value="//input[@id='login-button']")
+# Login
+button = driver.find_element(
+    By.XPATH,
+    "//input[@id='login-button']"
+)
 
-time.sleep(5)
 button.click()
+time.sleep(3)
 
-time.sleep(5)
+# Click Fleece Jacket
+product = driver.find_element(
+    By.XPATH,
+    "//div[text()='Sauce Labs Fleece Jacket']"
+)
+
+product.click()
+time.sleep(2)
+
+# Add Fleece Jacket to cart
+addcart = driver.find_element(
+    By.XPATH,
+    "//button[@id='add-to-cart-sauce-labs-fleece-jacket']"
+)
+
+addcart.click()
+time.sleep(2)
+
+# Remove Fleece Jacket
+remove = driver.find_element(
+    By.XPATH,
+    "//button[@id='remove-sauce-labs-fleece-jacket']"
+)
+
+remove.click()
+time.sleep(2)
+
 driver.quit()
