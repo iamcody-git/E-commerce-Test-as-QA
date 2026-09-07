3b8eefaceb9404d74d8648c68357c8f956ab8b7e
iamcody-git <iamcody.github@gmail.com>
2026-09-07 07:41:49 +0545

    explicit wait

--- selenium with pytest/tests/test_view.py
+++ selenium with pytest/tests/test_view.py
@@ -1,17 +1,40 @@
 from selenium.webdriver.common.by import By
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 def test_product(driver):
+
     driver.get("https://www.saucedemo.com/")
 
-    #username
-    driver.find_element(By.ID, "user-name").send_keys("standard_user")
-    driver.find_element(By.ID, "password").send_keys("secret_sauce")
-    driver.find_element(By.ID, "login-button").click()
-    time.sleep(2)
+    wait = WebDriverWait(driver, 10)
 
-    driver.find_element(By.ID,'add-to-cart-sauce-labs-bike-light').click()
+    # Username
+    wait.until(
+        EC.visibility_of_element_located((By.ID, "user-name"))
+    ).send_keys("standard_user")
 
-    time.sleep(5)
+    # Password
+    wait.until(
+        EC.visibility_of_element_located((By.ID, "password"))
+    ).send_keys("secret_sauce")
 
+    # Login button
+    wait.until(
+        EC.element_to_be_clickable((By.ID, "login-button"))
+    ).click()
+
+    # Wait until product page is loaded
+    wait.until(
+        EC.url_to_be("https://www.saucedemo.com/inventory.html")
+    )
+
+    # Add Bike Light to cart
+    wait.until(
+        EC.element_to_be_clickable(
+            (By.ID, "add-to-cart-sauce-labs-bike-light")
+        )
+    ).click()
+
+    # Final assertion
     assert driver.current_url == "https://www.saucedemo.com/inventory.html"
