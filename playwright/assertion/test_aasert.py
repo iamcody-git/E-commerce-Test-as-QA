95cda477e1797c62045633e6fe037bc438971de6
iamcody-git <iamcody.github@gmail.com>
2026-09-24 10:36:40 +0545

    assertion

--- playwright/assertion/test_aasert.py
+++ playwright/assertion/test_aasert.py
@@ -1,1 +1,6 @@
-print("assertion")
+from playwright.sync_api import Page
+
+def test_assertion(page:Page):
+    page.goto("https://www.google.com/")
+    assert page.title() == 'Google'
+    print('Googling')
